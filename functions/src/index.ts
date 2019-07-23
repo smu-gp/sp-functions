faabbdc4f7292043bd20e4d934042e5b602935b5
NamHyun Gu <mnhan0403@gmail.com>
2019-07-23 22:37:44 +0900

    Initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+
+admin.initializeApp();
+
+export const getToken = functions.https.onRequest((request, response) => {
+  if (request.method == "POST") {
+    if (request.body.uid) {
+      const uid = request.body.uid;
+      admin
+        .auth()
+        .createCustomToken(uid)
+        .then(customToken => {
+          response.send(customToken);
+        })
+        .catch(error => {
+          response.send(`err: ${error}`);
+        });
+    } else {
+      response.send("getToken is required uid value");
+    }
+  } else {
+    response.send("getToken is work only POST method.");
+  }
+});
