d8080ce8243531a2902471fabbb43182949cf4e0
NamHyun Gu <mnhan0403@gmail.com>
2019-08-08 22:45:32 +0900

    Enable cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as cors from "cors";
+
+const corsHandler = cors({ origin: true });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,22 +10,24 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export const getToken = functions.https.onRequest((request, response) => {
-  if (request.method == "POST") {
-    if (request.body.uid) {
-      const uid = request.body.uid;
-      admin
-        .auth()
-        .createCustomToken(uid)
-        .then(customToken => {
-          response.send(customToken);
-        })
-        .catch(error => {
-          response.send(`err: ${error}`);
-        });
+  corsHandler(request, response, () => {
+    if (request.method == "POST") {
+      if (request.body.uid) {
+        const uid = request.body.uid;
+        admin
+          .auth()
+          .createCustomToken(uid)
+          .then(customToken => {
+            response.send(customToken);
+          })
+          .catch(error => {
+            response.send(`err: ${error}`);
+          });
+      } else {
+        response.send("getToken is required uid value");
+      }
     } else {
-      response.send("getToken is required uid value");
+      response.send("getToken is work only POST method.");
     }
-  } else {
-    response.send("getToken is work only POST method.");
-  }
+  });
 });
